f639bec9ea564c7b907917c23ae2a3396b4c5c3f
hainguyencse <hai.nguyen@alphabase.vn>
2017-02-18 15:23:14 +0700

    Update settings

--- inspifood/settings.py
+++ inspifood/settings.py
@@ -110,5 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 MEDIA_URL = '/uploads/'
